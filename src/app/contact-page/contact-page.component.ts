d4b6614d4471a9ddaa9fa1e580bed6324152870f
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-11-22 23:31:31 +0100

    contact component works

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Listing} from "../types";
+import {ActivatedRoute, Router} from "@angular/router";
+
+import { fakeListings } from "../fake-data";
 
 @Component({
   selector: 'app-contact-page',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  message: string = '';
+  listing!: Listing;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id) as Listing;
+    this.message = `Hi, I'm interested in your ${this.listing.name}
+    `
+    console.log(this.message)
   }
 
 }
